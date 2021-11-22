90d29838b2d6c18755da20bcb80abefe58106433
jianan <jianan@gmail.com>
2021-11-22 15:03:22 +0800

    init

--- cart/resources/views/addProduct.blade.php
+++ cart/resources/views/addProduct.blade.php
@@ -1,49 +1,40 @@
 @extends('layout')
 @section('content')
 <div class="row">
-    <div class="co1-sm-3">
-
-    </div>
-    <div class="co1-sm-6">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-6">
         <br><br>
         <h3>Create New Product</h3>
         <form action="">
             <div class="form-group">
                 <label for="productName">Product Name</label>
-                <input type="text" class="form-control" id="productName" name="productName">
-
+                <input type="text" class="form-control" id="productName" name="productName">                
             </div>
             <div class="form-group">
-                <label for="productDescription">Product Description</label>
-                <input type="text" class="form-control" id="productDescription" name="productDescription">
-
+                <label for="productDescription">Product Desciption</label>
+                <input type="text" class="form-control" id="productDescription" name="productDescription">                
             </div>
             <div class="form-group">
                 <label for="productPrice">Product Price</label>
-                <input type="text" class="form-control" id="productPrice" name="productPrice" min="0">
-
+                <input type="number" class="form-control" id="productPrice" name="productPrice" min="0">                
             </div>
             <div class="form-group">
-                <label for="productQuantity">Product Quantity</label>
-                <input type="text" class="form-control" id="productQuantity" name="productQuantity" min="0">
+                <label for="productPrice">Product Quantity</label>
+                <input type="number" class="form-control" id="productQuantity" name="productQuantity" min="0">                
+            </div>
 
+            <div class="form-group">
+                <label for="productDescription">Product Image</label>
+                <input type="file" class="form-control" id="productImage" name="productImage">                
             </div>
             <div class="form-group">
                 <label for="catID">Category</label>
-                <input type="text" class="form-control" id="CategoryID" name="categoryID">
-
+                <input type="text" class="form-control" id="CategoryID" name="categoryID">                
             </div>
-            <div class="form-group">
-                <label for="productImage">Image</label>
-                <input type="file" class="form-control" id="CategoryID" name="categoryID">
-
-            </div>
-            <button type="submit">Add New</button>
+            <button type="submit" class="btn btn-primary">Add New</button>
         </form>
         <br><br>
     </div>
-    <div class="co1-sm-3">
-
-    </div>
+    <div class="col-sm-3"></div>
 </div>
 @endsection
